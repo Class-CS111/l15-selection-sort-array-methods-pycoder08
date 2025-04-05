481d225b7e74f1deac3f4192f98b45642736f569
Muhammad Conn <muhammad.conn@icloud.com>
2025-04-04 19:28:36 -0700

    Developed the requested methods, though did not develop any test cases as the instructions did not call for any

--- Main.java
+++ Main.java
@@ -9,6 +9,18 @@ public class Main
 {
   public static void main(String[] args)
   {
-
+    int[] stringArray = new int[11];
+    for (int i = 0; i < stringArray.length; i++)
+    {
+      stringArray[i] = i + 1;
+    }
+    System.out.println(ArrayMethods.arrayString(stringArray));
+    ArrayMethods.swap(stringArray, 0, 1);
+    System.out.println(ArrayMethods.arrayString(stringArray));
+    System.out.println(ArrayMethods.indexOfMin(stringArray, 0));
+    ArrayMethods.reverse(stringArray);
+    System.out.println(ArrayMethods.arrayString(stringArray));
+    ArrayMethods.selectionSort(stringArray);
+    System.out.println(ArrayMethods.arrayString(stringArray));
   }
 }
